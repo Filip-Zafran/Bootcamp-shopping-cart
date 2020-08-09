b393a9be79912fe71ae0cafeec9e75931ab63e1e
FichoZee <filip.zafran@gmail.com>
2020-08-09 21:56:55 +0200

    create nav and Home component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-  <header>
-        <nav>
-          <h2>Shop</h2>
-          <ul>
-            <li>
-              <a h href="#">Home</a>
-            </li>
-               <li>
-              <a h href="#">About</a>
-            </li>
-               <li>
-              <a h href="#">Cart</a>
-            </li>
-          </ul>
-        </nav>
-        </header>
+ <Home />
+
+
     </div>
   );
 }
